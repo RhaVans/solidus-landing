c537d827c7c051437521a902b3af8aad93e572ad
RhaVans <rhavans197@gmail.com>
2026-03-09 14:06:21 +0700

    remove: AUM Growth section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import VisiMisiSection from "@/components/sections/VisiMisi";
 import HistorySection from "@/components/sections/History";
 import StatisticsSection from "@/components/sections/Statistics";
 import PortfolioSection from "@/components/sections/Portfolio";
-import AUMGrowthSection from "@/components/sections/AUMGrowth";
 import TeamSection from "@/components/sections/Team";
 import ContactSection from "@/components/sections/Contact";
 
@@ -17,7 +16,6 @@ export default function Home() {
       <HistorySection />
       <StatisticsSection />
       <PortfolioSection />
-      <AUMGrowthSection />
       <TeamSection />
       <ContactSection />
     </div>
